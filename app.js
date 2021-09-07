7a0c4984b69cb34d46414b0a697c73bb3cc4ad4f
unknown <rakibulhasan.seu.cse@gmail.com>
2021-09-07 22:37:48 +0600

    All done

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 const searchBtn = document.getElementById('click-btn');
 const hexCodeContainer = document.getElementById('hex-code');
-
+// function
 const colorChange = () =>{
     const hexNumbers = ['0', '1', '2','3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
     let hexCode = [];
+    // loop will iterate 6times
     for(let i=0; i<6; i++){
         let randomIndex = Math.floor(Math.random()*hexNumbers.length);
-        console.log(randomIndex);
         hexCode+=hexNumbers[randomIndex];
-        console.log(hexCode);
     }
     hexCodeContainer.innerHTML = hexCode;
     document.getElementsByTagName('body')[0].style.background = '#'+hexCode;   
 }
+//event listener
 searchBtn.addEventListener('click', colorChange);
